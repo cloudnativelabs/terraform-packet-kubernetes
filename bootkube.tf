72a2a13d9a2ca9395db30c0544ac261aef93496f
bzub <bzubrod@gmail.com>
2017-08-21 15:29:37 -0500

    Add new option "api_ip" to bootkube module This allows us to hit the api server without DNS/hosts setup.

--- bootkube.tf
+++ bootkube.tf
@@ -1,10 +1,11 @@
 module "bootkube" {
-  source = "git://github.com/bzub/bootkube-terraform.git?ref=v0.5.1"
+  source = "git://github.com/cloudnativelabs/bootkube-terraform.git?ref=v0.6.1"
 
   cluster_name = "${var.cluster_name}"
 
   #TODO: Dynamically determine apiservers from nodes, set to a variable
   api_servers                   = ["${packet_device.controller.0.hostname}.${var.k8s_domain_name}"]
+  api_ip                        = "${packet_device.controller.0.ipv4_public}"
   asset_dir                     = "${var.asset_dir}"
   etcd_servers                  = ["${var.etcd_servers}"]
   experimental_self_hosted_etcd = "${var.experimental_self_hosted_etcd}"
