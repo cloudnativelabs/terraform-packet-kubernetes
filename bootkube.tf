e95cea63718786cfa4567787d3eacf010ca2b5f7
bzub <bzubrod@gmail.com>
2017-08-21 19:37:49 -0500

    Fix bootkube module input variable name

--- bootkube.tf
+++ bootkube.tf
@@ -5,7 +5,7 @@ module "bootkube" {
 
   #TODO: Dynamically determine apiservers from nodes, set to a variable
   api_servers                   = ["${packet_device.controller.0.hostname}.${var.server_domain}"]
-  api_ip                        = "${packet_device.controller.0.ipv4_public}"
+  api_servers_ips               = ["${packet_device.controller.0.ipv4_public}"]
   asset_dir                     = "${var.asset_dir}"
   etcd_servers                  = ["${var.etcd_servers}"]
   experimental_self_hosted_etcd = "${var.experimental_self_hosted_etcd}"
