c1c9e2cacdb645cd73c529a1ea03baacc444572d
bzub <bzubrod@gmail.com>
2017-08-22 19:37:44 -0500

    Support running kubectl.sh from any directory

--- kubectl.sh
+++ kubectl.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env sh
 set -e
 
-KUBE_VERSION="v$(terraform output kube_version_patch)"
-API_IP=$(terraform output api_ip)
-KUBECONFIG=$(terraform output kubeconfig_path)
+script_dir="$(CDPATH= cd -- "$(dirname -- "$0")" && pwd -P)"
+TERRAFORM_OUTPUT="terraform output --state=${script_dir}/terraform.tfstate"
+KUBE_VERSION="v$(eval "${TERRAFORM_OUTPUT} kube_version_patch")"
+API_IP="$(eval "${TERRAFORM_OUTPUT} api_ip")"
+KUBECONFIG="$(eval "${TERRAFORM_OUTPUT} kubeconfig_path")"
+export KUBECONFIG
 
 # TODO: Download appropriate kubectl version if needed to match cluster
 kubectl --server="https://${API_IP}" "${@}"
