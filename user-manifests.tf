4f95dfd2ccdce215aa43029958a3ea0416c94f67
bzub <bzubrod@gmail.com>
2017-08-23 22:13:44 -0500

    Remove user-manifests loading feature Users should use the provided kubectl.sh to manipulate the cluster instead.

--- user-manifests.tf
+++
@@ -1,9 +0,0 @@
-# Apply user manifests
-resource "null_resource" "user_manifests" {
-  depends_on = ["null_resource.bootkube_start"]
-
-  # TODO: Download kubectl that corresponds to k8s version and host arch.
-  provisioner "local-exec" {
-    command = "sleep 20s && KUBECONFIG=assets/auth/kubeconfig kubectl --server=https://${packet_device.controller.0.ipv4_public} apply -f user-manifests"
-  }
-}
