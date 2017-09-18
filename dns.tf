2328266fc0b13a886ac478fd16f6f9e846c0b249
bzub <bzubrod@gmail.com>
2017-09-18 00:26:14 -0500

    Use kubernetes provider to configure kube-dns This frees us from injecting the configmap manifest into the bootkube assets/manifests directory, which was causing a loop of state changes every time a user refreshes/applies resources.

--- dns.tf
+++ dns.tf
@@ -1,13 +1,17 @@
-data "template_file" "kube_dns_cfg" {
-  template = "${file("${path.module}/templates/kube-dns-cfg.yaml")}"
-
-  vars {
-    server_domain = "${var.server_domain}"
-  }
+provider "kubernetes" {
+  config_path = "${module.bootkube.user-kubeconfig}"
+  config_context = "test-context"
 }
 
-resource "local_file" "kube_dns_cfg" {
-  depends_on = ["module.bootkube"]
-  content    = "${data.template_file.kube_dns_cfg.rendered}"
-  filename   = "${path.module}/${var.asset_dir}/manifests/kube-dns-cfg.yaml"
+resource "kubernetes_config_map" "kube_dns" {
+  metadata {
+    name = "kube-dns"
+    namespace = "kube-system"
+  }
+
+  data {
+    upstreamNameservers = <<EOF
+["8.8.8.8", "8.8.4.4"]
+EOF
+  }
 }
