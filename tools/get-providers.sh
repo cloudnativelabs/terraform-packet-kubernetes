f6a74fa3db13b18c13db952f582cac61df540702
bzub <bzubrod@gmail.com>
2017-10-01 11:39:00 -0500

    Use docker to build provider(s)
    Provides a clean Go workspace.

--- tools/get-providers.sh
+++ tools/get-providers.sh
@@ -5,7 +5,25 @@ if [ ! -d "${GOPATH}" ]; then
     echo "ERROR: GOPATH is not set up. Do you have a functional Go workspace?"
 fi
 
+script_dir="$(CDPATH= cd -- "$(dirname -- "$0")" && pwd -P)"
+
+if [ "${IN_DOCKER_CONTAINER}" != "1" ]; then
+    docker pull golang:alpine
+    docker run \
+        --rm \
+        -v "${script_dir}/$(basename ${0}):/get-providers.sh" \
+        -v "${GOPATH}/bin:/go/bin" \
+        -e "IN_DOCKER_CONTAINER=1" \
+        golang:alpine \
+        sh -c " \
+            apk add -U git && \
+            /get-providers.sh \
+        "
+    exit 0
+fi
+
 CGO_ENABLED=0
+export CGO_ENABLED
 
 set_provider_vars() {
     for i in ${@}; do
@@ -29,30 +47,41 @@ print_providers() {
 }
 
 go_get_providers() {
-    git_alias_date="$(date --utc --iso-8601=seconds)"
+    git_alias_date="$(date --utc -Iseconds)"
     for i in ${provider_go_urls}; do
         short_name="$(provider_short_name "${i}")"
         git_alias="$(eval 'echo ${'${short_name}'}')"
+        go get -d "${i}"
         if [ -n "${git_alias}" ]; then
             echo "INFO: Using ${git_alias} as git alias for $(provider_full_name "${i}")"
 
-            if [ -d "$GOPATH/src/${i}" ]; then
-                echo "INFO: Moving $GOPATH/src/${i} -> $GOPATH/src/${i}-${git_alias_date}"
-                mv "$GOPATH/src/${i}" "$GOPATH/src/${i}-${git_alias_date}"
-            fi
+            # if [ -d "$GOPATH/src/${i}" ]; then
+            #     echo "INFO: Moving $GOPATH/src/${i} -> $GOPATH/src/${i}-${git_alias_date}"
+            #     mv "$GOPATH/src/${i}" "$GOPATH/src/${i}-${git_alias_date}"
+            # fi
 
-            echo "INFO: Fetching alias git repo $GOPATH/src/${git_alias}"
-            go get -d "${git_alias}"
-            cp -r "$GOPATH/src/${git_alias}" "$GOPATH/src/${i}"
+            echo "INFO: Fetching alias git repo ${git_alias}"
+            old_dir="${PWD}"
+            cd "$GOPATH/src/${i}"
+            git remote add alias_repo "${git_alias}"
+            git fetch alias_repo
+            git checkout -b alias_repo_master alias_repo/master
+            go install
+            cd "${old_dir}"
+            # go get -d "${git_alias}"
+            # ln -s "$GOPATH/src/${git_alias}" "$GOPATH/src/${i}"
+            # old_dir="${PWD}"
+            # cd "$GOPATH/src/${i}"
+            # git pull
+            # go install
+            # cd "${old_dir}"
         fi
 
-        go get -u "${i}"
-
-        if [ -n "${git_alias}" ] && [ -d "$GOPATH/src/${i}-${git_alias_date}" ]; then
-            echo "INFO: Restoring $GOPATH/src/${i}-${git_alias_date} to original location."
-            rm -rf "$GOPATH/src/${i}"
-            mv "$GOPATH/src/${i}-${git_alias_date}" "$GOPATH/src/${i}"
-        fi
+        # if [ -n "${git_alias}" ] && [ -d "$GOPATH/src/${i}-${git_alias_date}" ]; then
+        #     echo "INFO: Restoring $GOPATH/src/${i}-${git_alias_date} to original location."
+        #     unlink "$GOPATH/src/${i}"
+        #     mv "$GOPATH/src/${i}-${git_alias_date}" "$GOPATH/src/${i}"
+        # fi
     done
 }
 
@@ -69,11 +98,11 @@ print_terraformrc() {
 ### main ###
 
 # Set provider git repo aliases
-packet="github.com/cloudnativelabs/terraform-provider-packet"
+# packet="github.com/cloudnativelabs/terraform-provider-packet"
+packet="https://github.com/cloudnativelabs/terraform-provider-packet.git"
 export packet
 
 set_provider_vars \
-    "github.com/coreos/terraform-provider-ct" \
     "github.com/terraform-providers/terraform-provider-packet"
 
 echo "INFO: ${0} will install the following Terraform providers:"
@@ -87,23 +116,6 @@ echo
 echo "INFO: Generating .terraformrc content."
 terraformrc="$(print_terraformrc)"
 echo
-
-# backup_date="$(date --utc --iso-8601=seconds)"
-# if [ -e "${HOME}/.terraformrc" ]; then
-#     if [ "${terraformrc}" = "$(cat "${HOME}/.terraformrc")" ]; then
-#         echo "INFO: ${HOME}/.terraformrc already looks good! Exiting."
-#         exit 0
-#     else
-#         echo "INFO: Backing up existing .terraformrc to ${HOME}/.terraformrc-${backup_date}"
-#         if [ -f "${HOME}/.terraformrc" ]; then
-#             mv "${HOME}/.terraformrc" "${HOME}/.terraformrc-${backup_date}"
-#             echo
-#         else
-#             echo "ERROR: Backup failed. ${HOME}/.terraformrc is not a file?"
-#             exit 1
-#         fi
-#     fi
-# fi
 
 echo "Done! Be sure you have the following in your ~/.terraformrc"
 echo "${terraformrc}"
